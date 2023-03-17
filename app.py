2c377643388286fcd1710719d43eec261e46e6e4
neharaut00 <neharaut372@gmail.com>
2023-03-18 00:12:14 +0530

    second commit

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import tensorflow as tf
 from keras.preprocessing import image
 from keras.models import load_model
 from keras.utils import img_to_array
@@ -12,25 +13,45 @@ import sys
 # Loading model
 model = load_model("F:/Quark/ML workshop/ML workshop project/vegetables.h5")
 
-class_map = {0: 'Bean', 1: 'Bitter_Gourd', 2: 'Bottle_Gourd', 3: 'Brinjal', 4: 'Broccoli', 5: 'Cabbage', 6: 'Capsicum', 7: 'Carrot', 8: 'Cauliflower', 9: 'Cucumber', 10: 'Papaya', 11: 'Potato', 12: 'Pumpkin', 13: 'Radish', 14: 'Tomato'}
+class_map = ['Bean', 'Bitter Gourd', 'Bottle Gourd', 'Brinjal', 'Broccoli', 'Cabbage', 'Capsicum', 'Carrot', 'Cauliflower', 'Cucumber', 'Papaya', 'Potato', 'Pumpkin', 'Radish', 'Tomato']
 
 # Preparing and pre-processing the image
-def preprocess_predict_img(img_path):  
-    print(1)
-    print(img_path)
-    test_img = image.load_img(img_path, target_size=(150, 150))
-    print(2)
-    test_img_arr = image.img_to_array(test_img)/255.0
-    print(3)
-    test_img_input = test_img_arr.reshape((1, test_img_arr.shape[0], test_img_arr.shape[1], test_img_arr.shape[2]))
-    print(4)
-    predicted_label = np.argmax(model.predict(test_img_input))
-    print(5)
-    predicted_vegetable = class_map[predicted_label]
-    print(6)
-    return predicted_vegetable
+def load_and_prep_image(img_path):  
+    """
+    Reads an image from filename, turns it into a tensor
+    and reshapes it to (img_shape, img_shape, colour_channel).
+    """
+    # Read in target file (an image)
+    img = tf.io.read_file(img_path)
 
+    # Decode the read file into a tensor & ensure 3 colour channels 
+    # (our model is trained on images with 3 colour channels and sometimes images have 4 colour channels)
+    img = tf.image.decode_image(img, channels=3)
 
+    # Resize the image (to the same size our model was trained on)
+    img = tf.image.resize(img, size = [150, 150])
+
+    # Rescale the image (get all values between 0 and 1)
+    img = img/255.
+    # print(img)
+    return img
+
+def pred_and_plot(filename, model = model):
+  """
+  Imports an image located at filename, makes a prediction on it with
+  a trained model and plots the image with the predicted class as the title.
+  """
+  # Import the target image and preprocess it
+  img = load_and_prep_image(filename)
+  print("predict")
+
+  # Make a prediction
+  pred = model.predict(tf.expand_dims(img, axis=0))
+
+  # Get the predicted class
+  pred_class = class_map[int(tf.round(pred)[0][0])]
+
+  return pred_class
 # Instantiating flask app
 app = Flask(__name__)
 
@@ -49,9 +70,9 @@ def predict_image_file():
             img_path = os.path.join(basepath, 'uploads', secure_filename(img.filename))
             img.save(img_path)
             # print(img_path, file=sys.stderr)            
-            print("Above Prediction", file=sys.stderr)
-            pred = preprocess_predict_img(img_path)
-            print("Prediction: "+pred, file=sys.stderr)
+            # print("Above Prediction", file=sys.stderr)
+            pred = pred_and_plot(img_path)
+            # print("Prediction: "+pred, file=sys.stderr)
             return render_template("result.html", predictions=pred, img_path = img_path)
 
     except:
